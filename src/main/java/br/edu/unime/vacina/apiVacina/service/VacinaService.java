1b00c81130d01eb61def70643d24f8382b4f738d
Xalvitor <68449900+Xalvitor@users.noreply.github.com>
2023-10-13 21:37:22 -0300

    Adição de cache
    Adição de CacheConfig para criar o cache em si e adicionado o uso de cache em VacinaService

--- src/main/java/br/edu/unime/vacina/apiVacina/service/VacinaService.java
+++ src/main/java/br/edu/unime/vacina/apiVacina/service/VacinaService.java
@@ -4,6 +4,10 @@ import br.edu.unime.vacina.apiVacina.entity.Log;
 import br.edu.unime.vacina.apiVacina.entity.Vacina;
 import br.edu.unime.vacina.apiVacina.repository.VacinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +23,13 @@ public class VacinaService {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
+    @Autowired
+    public VacinaService(CacheManager cacheManager, VacinaRepository vacinaRepository) {
+        this.cacheManager = cacheManager;
+        this.vacinaRepository = vacinaRepository;
+    }
+    private final CacheManager cacheManager;
 
     public void registrarLog(String metodo, String acao, String mensagem, int statusCode) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -41,7 +52,18 @@ public class VacinaService {
         return vacinaRepository.findAll();
     }
 
+    @Cacheable("vacinaCache")
     public Vacina encontrarVacina(String id) throws Exception {
+        Cache cache = cacheManager.getCache("vacinaCache");
+
+        if (cache != null){
+            Cache.ValueWrapper valorBuscaId = cache.get(id);
+            if (valorBuscaId != null) {
+                Vacina vacina = (Vacina) valorBuscaId.get();
+                return vacina;
+            }
+        }
+
         Optional<Vacina> vacinaOptional = vacinaRepository.findById(id);
 
         if (vacinaOptional.isEmpty()) {
@@ -51,11 +73,14 @@ public class VacinaService {
         return vacinaOptional.get();
     }
 
+    @CachePut("vacinaCache")
     public void inserir(Vacina vacina){
         vacinaRepository.insert(vacina);
     }
 
+    @CachePut(value = "vacinaCache", key = "#id")
     public Vacina atualizar(String id, Vacina vacina) throws Exception {
+
         Vacina vacinaAntigo = encontrarVacina(id);
 
         vacinaAntigo.setFabricante(vacina.getFabricante());
@@ -70,10 +95,14 @@ public class VacinaService {
     }
 
     public void deletar(String id) throws Exception {
+        Cache cache = cacheManager.getCache("vacinaCache");
+        if (cache != null) {
+            cache.evict(id);
+        }
+
         Vacina vacina = encontrarVacina(id);
 
         vacinaRepository.delete(vacina);
-
     }
 
 }
