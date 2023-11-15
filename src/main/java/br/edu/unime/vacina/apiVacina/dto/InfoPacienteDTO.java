eba84299c15d37b2e37f8386d7e0bf34b4ed192f
Xalvitor <68449900+Xalvitor@users.noreply.github.com>
2023-11-15 18:55:01 -0300

    Alterações de validação
    Alterações feitas de validção

---
+++ src/main/java/br/edu/unime/vacina/apiVacina/dto/InfoPacienteDTO.java
@@ -0,0 +1,9 @@
+package br.edu.unime.vacina.apiVacina.dto;
+
+public class InfoPacienteDTO {
+    public String nome;
+    public int idade;
+    public String bairro;
+    public String municipio;
+    public String estado;
+}
